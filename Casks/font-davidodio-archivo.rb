147174f3637194625449d3344afd1e771ed99773
David ODonnell <code@davidod.io>
2020-02-10 12:40:02 +0100

    fix: correct font information and zip

--- Casks/font-davidodio-archivo.rb
+++ Casks/font-davidodio-archivo.rb
@@ -1,6 +1,6 @@
 cask 'font-davidodio-archivo' do
-  version :latest
-  sha256 :no_check
+  version '1.004'
+  sha256 '48f42e25ab0ce8a0d59ea04b99b338e6dd4667bf925fbeddd34958dcb814512f'
 
   url 'https://github.com/davidodio/homebrew-extras/blob/master/Fonts/Archivo.zip'
   name 'Archivo'
@@ -11,9 +11,9 @@ cask 'font-davidodio-archivo' do
   font 'Archivo-Bold.ttf'
   font 'Archivo-BoldItalic.ttf'
   font 'Archivo-Italic.ttf'
-	font 'Archivo-Medium.ttf'
-	font 'Archivo-MediumItalic.ttf'
-	font 'Archivo-Regular.ttf'
-	font 'Archivo-SemiBold.ttf'
-	font 'Archivo-SemiBoldItalic.ttf'
+  font 'Archivo-Medium.ttf'
+  font 'Archivo-MediumItalic.ttf'
+  font 'Archivo-Regular.ttf'
+  font 'Archivo-SemiBold.ttf'
+  font 'Archivo-SemiBoldItalic.ttf'
 end
