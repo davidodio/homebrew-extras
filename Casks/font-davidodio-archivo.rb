87ef56cd53e4716e6dbe93aa3de183b04d128085
David ODonnell <code@davidod.io>
2020-02-10 14:33:07 +0100

    fix: font file extensions

--- Casks/font-davidodio-archivo.rb
+++ Casks/font-davidodio-archivo.rb
@@ -8,12 +8,12 @@ cask 'font-davidodio-archivo' do
 
   depends_on macos: '>= :sierra'
 
-  font 'Archivo/otf/Archivo-Bold.ttf'
-  font 'Archivo/otf/Archivo-BoldItalic.ttf'
-  font 'Archivo/otf/Archivo-Italic.ttf'
-  font 'Archivo/otf/Archivo-Medium.ttf'
-  font 'Archivo/otf/Archivo-MediumItalic.ttf'
-  font 'Archivo/otf/Archivo-Regular.ttf'
-  font 'Archivo/otf/Archivo-SemiBold.ttf'
-  font 'Archivo/otf/Archivo-SemiBoldItalic.ttf'
+  font 'Archivo/otf/Archivo-Bold.otf'
+  font 'Archivo/otf/Archivo-BoldItalic.otf'
+  font 'Archivo/otf/Archivo-Italic.otf'
+  font 'Archivo/otf/Archivo-Medium.otf'
+  font 'Archivo/otf/Archivo-MediumItalic.otf'
+  font 'Archivo/otf/Archivo-Regular.otf'
+  font 'Archivo/otf/Archivo-SemiBold.otf'
+  font 'Archivo/otf/Archivo-SemiBoldItalic.otf'
 end
