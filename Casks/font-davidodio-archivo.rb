8676b539ff2d56ad657d89bb647ddb5b0dfd93a3
David ODonnell <code@davidod.io>
2020-02-10 12:45:12 +0100

    fix: checksum

--- Casks/font-davidodio-archivo.rb
+++ Casks/font-davidodio-archivo.rb
@@ -1,6 +1,7 @@
 cask 'font-davidodio-archivo' do
   version '1.004'
-  sha256 '48f42e25ab0ce8a0d59ea04b99b338e6dd4667bf925fbeddd34958dcb814512f'
+  # sha256 '48f42e25ab0ce8a0d59ea04b99b338e6dd4667bf925fbeddd34958dcb814512f'
+  sha256 '1dcffe052e1a8bc20fdd779d31e4274727e4e9e682fa7f0be84a15886b740ec3'
 
   url 'https://github.com/davidodio/homebrew-extras/blob/master/Fonts/Archivo.zip'
   name 'Archivo'
