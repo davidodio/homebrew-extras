434c9389b9aa2e48b0ae5a1063d5be7f79e98c48
David ODonnell <code@davidod.io>
2020-02-10 12:02:49 +0100

    feat: add archivo font

---
+++ Casks/font-davidodio-archivo.rb
@@ -0,0 +1,19 @@
+cask 'font-davidodio-archivo' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://github.com/davidodio/homebrew-extras/blob/master/Fonts/archivo.zip'
+  name 'Archivo'
+  homepage 'https://www.google.com/fonts/specimen/Archivo'
+
+  depends_on macos: '>= :sierra'
+
+  font 'Archivo-Bold.ttf'
+  font 'Archivo-BoldItalic.ttf'
+  font 'Archivo-Italic.ttf'
+	font 'Archivo-Medium.ttf'
+	font 'Archivo-MediumItalic.ttf'
+	font 'Archivo-Regular.ttf'
+	font 'Archivo-SemiBold.ttf'
+	font 'Archivo-SemiBoldItalic.ttf'
+end
